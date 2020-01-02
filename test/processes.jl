14bb529acc69c8d10a040c10565d4934cd6870a0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-01-01 21:46:59 -0300

    Add more tests

--- test/processes.jl
+++ test/processes.jl
@@ -1,10 +1,19 @@
 @testset "Processes" begin
+  @testset "Basic" begin
+    for p in [BinomialProcess(100), PoissonProcess(100.)]
+      r = RectangleRegion((0., 1.), (1., 2.))
+      P = rand(p, r)
+      X = coordinates(P)
+      @test all(0 .≤ X[1,:] .≤ 1)
+      @test all(1 .≤ X[2,:] .≤ 2)
+    end
+  end
+
   @testset "Binomial" begin
-    p = BinomialProcess(100)
-    r = RectangleRegion((0., 1.), (1., 2.))
-    P = rand(p, r)
-    X = coordinates(P)
-    @test all(0 .≤ X[1,:] .≤ 1)
-    @test all(1 .≤ X[2,:] .≤ 2)
+    # TODO
+  end
+
+  @testset "Poisson" begin
+    # TODO
   end
 end
