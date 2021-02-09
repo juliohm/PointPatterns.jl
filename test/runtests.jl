061e3a77a010628f8214ad86a4d2c7db3b4acae0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-02-09 20:32:33 -0300

    Add test/Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,6 @@
 using PointPatterns
 using GeoStatsBase
-using Plots, VisualRegressionTests
-using Test, Pkg, Random
-
-# workaround GR warnings
-ENV["GKSwstype"] = "100"
-
-# environment settings
-isCI = "CI" ∈ keys(ENV)
-islinux = Sys.islinux()
-visualtests = !isCI || (isCI && islinux)
-if !isCI
-  Pkg.add("Gtk")
-  using Gtk
-end
-datadir = joinpath(@__DIR__,"data")
+using Test, Random
 
 # list of tests
 testfiles = [
