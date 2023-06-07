7ff7e0b4a5abb8206cf00e4414477b027472d898
Dr. Erick A. Chacón Montalván <eral.th07@gmail.com>
2023-06-07 08:53:05 -0500

    Update src/processes/poisson.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/processes/poisson.jl
+++ src/processes/poisson.jl
@@ -6,9 +6,9 @@
    PoissonProcess(λ)
 
 A Poisson process with intensity `λ`. For a homogeneous process,
-define `λ` as a constant value, while for an inhomogeneous process,
-define `λ` as a Function or Vector. If `λ` is a vector, it is
-assumed that the process is associated with a `Mesh` with the same
+define `λ` as a constant real value, while for an inhomogeneous process,
+define `λ` as a function or vector of values. If `λ` is a vector, it is
+assumed that the process is associated with a `Domain` with the same
 number of elements as `λ`.
 """
 
