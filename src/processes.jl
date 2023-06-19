44f7d585b7b61cb716391a67f8b7441d914e070a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-06-19 14:01:43 -0300

    Refactoring

--- src/processes.jl
+++ src/processes.jl
@@ -79,12 +79,40 @@ or piecewise constant over a `Domain`.
 """
 struct ConstantIntensity <: PointPatternAlgo end
 
-struct UnionSampling <: PointPatternAlgo end
-
 #-----------------
 # IMPLEMENTATIONS
 #-----------------
 
 include("processes/binomial.jl")
 include("processes/poisson.jl")
-include("processes/union.jl")
+
+# ----------------
+# UNION PROCESSES
+# ----------------
+
+"""
+    UnionProcess(p₁, p₂)
+
+Union (or superposition) of spatial point processes `p₁` and `p₂`.
+"""
+struct UnionProcess{P₁<:PointProcess,P₂<:PointProcess} <: PointProcess
+  p₁::P₁
+  p₂::P₂
+end
+
+"""
+    p₁ ∪ p₂
+
+Return the union of point processes `p₁` and `p₂`.
+"""
+Base.union(p₁::PointProcess, p₂::PointProcess) = UnionProcess(p₁, p₂)
+
+ishomogeneous(p::UnionProcess) = ishomogeneous(p.p₁) && ishomogeneous(p.p₂)
+
+default_sampling_algorithm(::UnionProcess, ::Any) = nothing
+
+function rand_single(rng::Random.AbstractRNG, p::UnionProcess, g, ::Nothing)
+  pp₁ = rand(rng, p.p₁, g)
+  pp₂ = rand(rng, p.p₂, g)
+  PointSet([coordinates.(pp₁); coordinates.(pp₂)])
+end
