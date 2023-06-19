c8fad6a5ff0c89922fc96507b786a4f8a0e2cc68
juliohm <juliohm@users.noreply.github.com>
2023-06-19 11:53:08 +0000

    :robot: Format .jl files

--- test/processes.jl
+++ test/processes.jl
@@ -1,14 +1,14 @@
 @testset "Processes" begin
-  seg = Segment((0.0,0.0), (11.3,11.3))
+  seg = Segment((0.0, 0.0), (11.3, 11.3))
   tri = Triangle((0.0, 0.0), (5.65, 0.0), (5.65, 5.65))
-  quad = Quadrangle((0.0,0.0), (0.0,4.0), (4.0, 4.0), (4.0, 0.0))
-  box = Box((0.0,0.0), (4.0, 4.0))
-  ball = Ball((1.0,1.0), 2.25)
-  outer = Point2[(0,-4),(4,-1),(4,1.5),(0,3)]
-  hole1 = Point2[(0.2,-0.2),(1.4,-0.2),(1.4,0.6),(0.2,0.6)]
-  hole2 = Point2[(2,-0.2),(3,-0.2),(3,0.4),(2,0.4)]
+  quad = Quadrangle((0.0, 0.0), (0.0, 4.0), (4.0, 4.0), (4.0, 0.0))
+  box = Box((0.0, 0.0), (4.0, 4.0))
+  ball = Ball((1.0, 1.0), 2.25)
+  outer = Point2[(0, -4), (4, -1), (4, 1.5), (0, 3)]
+  hole1 = Point2[(0.2, -0.2), (1.4, -0.2), (1.4, 0.6), (0.2, 0.6)]
+  hole2 = Point2[(2, -0.2), (3, -0.2), (3, 0.4), (2, 0.4)]
   poly = PolyArea(outer, [hole1, hole2])
-  grid = CartesianGrid((0,0), (4,4), dims = (10, 10))
+  grid = CartesianGrid((0, 0), (4, 4), dims=(10, 10))
   points = Point2[(0, 0), (4.5, 0), (0, 4.2), (4, 4.3), (1.5, 1.5)]
   connec = connect.([(1, 2, 5), (2, 4, 5), (4, 3, 5), (3, 1, 5)], Triangle)
   mesh = SimpleMesh(points, connec)
@@ -48,11 +48,11 @@
       pp = rand(p, g)
       @test all(∈(g), pp)
       # custom thinnedsampling using λmax
-      pp = rand(p, g, algo = PointPatterns.ThinnedSampling(λ(Point2(12.0, 12.0))))
+      pp = rand(p, g, algo=PointPatterns.ThinnedSampling(λ(Point2(12.0, 12.0))))
       @test all(∈(g), pp)
       # discretizedsampling
       g = discretize(g)
-      pp = rand(p, g, algo = PointPatterns.DiscretizedSampling())
+      pp = rand(p, g, algo=PointPatterns.DiscretizedSampling())
       @test all(∈(g), g)
     end
 
