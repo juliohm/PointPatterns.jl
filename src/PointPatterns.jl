7c06f8524771a78c7e080ac2463ed1359630a5b1
Erick <eral.th07@gmail.com>
2023-06-23 00:55:19 -0500

    start with PoissonClusterProcess

--- src/PointPatterns.jl
+++ src/PointPatterns.jl
@@ -19,6 +19,7 @@ export
   PointProcess,
   BinomialProcess,
   PoissonProcess,
+  PoissonClusterProcess,
   UnionProcess,
   ishomogeneous,
 
---
+++ src/processes/clustered.jl
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
+"""
+   PoissonClusterProcess(p, o, dₒ)
+
+A Poisson Cluster process with Poisson process `p` for parents and with
+PoissonProcess `o` for offsprings. The offspring domain for each parent
+point is computed by evaluating the function `dₒ` over the parent point.
+"""
+struct PoissonClusterProcess{P<:PoissonProcess,F<:Function} <: PointProcess
+  p::P
+  o::P
+  dₒ::F
+end
+
+default_sampling_algorithm(::PoissonClusterProcess, ::Any) = ConstantIntensity()
+
+function rand_single(rng::Random.AbstractRNG, p::PoissonClusterProcess, g, ::ConstantIntensity)
+  # generate parents
+  parents = rand(p.p, g)
+
+  # generate offspring
+  offspringvec = [rand(p.o, p.dₒ(parent)).geoms for parent in parents]
+  offspringall = PointSet(vcat(offspringvec...))
+  offspring = view(offspringall, g)
+  PointSet(collect(offspring))
+end
+
+
