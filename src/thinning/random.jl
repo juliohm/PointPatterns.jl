d216e4f91e989265ca3ea667ddc4f5177377ce1c
JakeGrainger <j.p.grainger2@outlook.com>
2023-09-26 15:00:53 +0200

    Update existing processes Now return point pattern interface

--- src/thinning/random.jl
+++ src/thinning/random.jl
@@ -36,14 +36,29 @@ thin(p::PoissonProcess{<:Function}, t::RandomThinning{<:Real}) = PoissonProcess(
 # THINNING POINT PATTERN
 # -----------------------
 
-function thin(pp::PointSet, t::RandomThinning{<:Real})
-  draws = rand(Bernoulli(t.p), nelements(pp))
-  inds = findall(isequal(1), draws)
+function thin(pp::PointPattern, t::ThinningMethod)
+  inds = thinning_index(pp.points, pp.marks, t)
+   points = collect(view(pp.points, inds))
+  marks = if pp.marks === nothing
+    nothing
+  else
+    collect(view(pp.marks, inds))
+  end
+  PointPattern(PointSet(points), pp.region, marks)
+end
+
+function thin(pp::PointSet, t::ThinningMethod)
+  inds = thinning_index(pp, nothing, t)
   points = collect(view(pp, inds))
   PointSet(points)
 end
 
-function thin(pp::PointSet, t::RandomThinning{<:Function})
+function thinning_index(pp::PointSet, marks::Nothing, t::RandomThinning{<:Real})
+  draws = rand(Bernoulli(t.p), nelements(pp))
+  findall(isequal(1), draws)
+end
+
+function thinning_index(pp::PointSet, marks::Nothing, t::RandomThinning{<:Function})
   inds = Vector{Int}()
   for j in 1:nelements(pp)
     x = element(pp, j)
@@ -51,6 +66,5 @@ function thin(pp::PointSet, t::RandomThinning{<:Function})
       push!(inds, j)
     end
   end
-  points = collect(view(pp, inds))
-  PointSet(points)
+  return inds
 end
